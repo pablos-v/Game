74803aaeef8d37c8efc3cc55e566ca61c197faa4
Pablos <pablos@list.ru>
2022-02-21 20:36:31 +0300

    working code with comments, change API for your bot!

--- commands.py
+++ commands.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from random import randint
 
-database = {}
+database = {} # will be like {ID:[Name,bank]}
 
 
 def help(update: Update, context: CallbackContext):
@@ -11,26 +11,31 @@ Last hand wins.\n To start the game just send /run')
 
 
 def run(update: Update, context: CallbackContext):
-    if update.effective_user.id in database:
-        if database[update.effective_user.id][1] <= 0:
-            database[update.effective_user.id][1] = 21
+    if update.effective_user.id in database: # Do we know him?
+        if database[update.effective_user.id][1] <= 0: # If he played and bank is empty
+            database[update.effective_user.id][1] = 21 # He will play again
+        # If player continues game after pause - we remember the last bank TODO add greetings by name
         update.message.reply_text(f'Trere are {database[update.effective_user.id][1]} matches in the heap,\n send /turn and the number of matches you want to take.')
-    else:
-        database.update({update.effective_user.id : [update.effective_user.first_name, 21]})
+    else: # If he is new player - we remember him and add bank 21 match
+        database.update({update.effective_user.id : [update.effective_user.first_name, 21]}) # TODO add params to count wins and losses
+        # TODO add greetings by name
         update.message.reply_text(f'Trere are {database[update.effective_user.id][1]} matches in the heap,\n send /turn and the number of matches you want to take.')
 
 
 def turn(update: Update, context: CallbackContext):
-    msg = update.message.text
-    database[update.effective_user.id][1] -= int(msg.split()[1])
+    msg = update.message.text # read user`s input
+    database[update.effective_user.id][1] -= int(msg.split()[1]) # count bank after user`s move TODO check 1 <= input <= 4
     if database[update.effective_user.id][1] <= 0:
         update.message.reply_text(f'You take the last one and win the game!\n Type /run to play again.')
+        # TODO count wins
         return
     update.message.reply_text(f'{database[update.effective_user.id][1]} matches left, bot`s turn.')
+
     bot_take = bot_logic(database[update.effective_user.id][1])
     database[update.effective_user.id][1] -= bot_take
     if database[update.effective_user.id][1] <= 0:
         update.message.reply_text(f'Bot has taken the last one and wins the game... You loose.\n Type /run to play again!')
+        # TODO count losses
         return
     update.message.reply_text(f'Bot takes {bot_take} matches, {database[update.effective_user.id][1]} matches left.')
 
--- matches_bot.py
+++ matches_bot.py
@@ -6,9 +6,11 @@ import commands as c
 
 updater = Updater('5293815590:AAES4pa-TLVjgbu8zNp9fn6f19uboSJ1Lek')
 print("Server started")
+# TODO check the input and show help for every input without commands
 updater.dispatcher.add_handler(CommandHandler('help', c.help))
 updater.dispatcher.add_handler(CommandHandler('run', c.run))
 updater.dispatcher.add_handler(CommandHandler('turn', c.turn))
+# TODO make method showing user`s win/loss statistics
 
 updater.start_polling()
 updater.idle()
