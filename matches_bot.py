293549bd8eca85840327609598387980b8dc521f
Marina F <fedma001@mail.ru>
2022-02-22 15:32:21 +0300

    added adding of user when any command printed

--- matches_bot.py
+++ matches_bot.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 import commands as c
 
 
@@ -7,9 +7,11 @@ import commands as c
 updater = Updater('5110228065:AAGeoqKoZEpzIhU5GD_b76QLXmuf01uwp9I')
 print("Server started")
 # TODO check the input and show help for every input without commands
+
 updater.dispatcher.add_handler(CommandHandler('help', c.help))
 updater.dispatcher.add_handler(CommandHandler('run', c.run))
 updater.dispatcher.add_handler(CommandHandler('turn', c.turn))
+updater.dispatcher.add_handler(MessageHandler(Filters.command, c.unknown))
 # TODO make method showing user`s win/loss statistics
 
 updater.start_polling()
